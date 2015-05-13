77856d51fb872d5d3abf80e2739be1eb82b644f5
Abhishek Pareek <makeittotop@gmail.com>
2015-05-13 21:17:25 +0400

    Added python client

---
+++ twilio-sms-client.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import requests
+import sys
+
+to = sys.argv[1]
+
+data = {'From': "+14805265812", 'To': to, "Body": "Test", "StatusCallback": "http://106.187.50.144:9999/twilio"}
+
+auth=("AC748702fc738d6f16455dd418b93a67d1", "5b6293592a8c4a8e6ba2a36ea6ce1ec8")
+
+headers={'Host': 'api.twilio.com', 'Content-Type': 'application/x-www-form-urlencoded', 'Accept': "*/*", 'Method': 'POST'}
+
+url = "https://api.twilio.com/2010-04-01/Accounts/AC748702fc738d6f16455dd418b93a67d1/Messages.json"
+
+resp = requests.post(url, auth=auth, headers=headers, data=data)
+
+print resp.json()
